567e7b0d7147ff880bbf38a6fd8d8b629a336990
Mohammad Arkan <mdarkanurl@gmail.com>
2026-09-06 14:38:43 +0600

    Completed the project

--- main.go
+++ main.go
@@ -2,31 +2,59 @@ package main
 
 import "fmt"
 
-func decimalToBinary(num int) int {
-	return num
+func decimalToBinary(n int) string {
+	if n == 0 {
+		return "0"
+	}
+
+	binary := ""
+
+	for n > 0 {
+		remainder := n % 2
+		binary = fmt.Sprint(remainder) + binary
+		n = n / 2
+	}
+
+	return binary
 }
 
-func binaryToDecimal(num int) int {
-	return num
+func binaryToDecimal(binary string) int {
+	decimal := 0
+	power := 1
+
+	for i := len(binary) - 1; i >= 0; i-- {
+		if binary[i] == '1' {
+			decimal += power
+		}
+
+		power *= 2
+	}
+
+	return decimal
 }
 
 func main() {
-	var UserChoice string
+	for {
+		var UserChoice string
 
-	fmt.Print("What types of number do you convert: ")
-	fmt.Scanln(&UserChoice)
+		fmt.Print("What types of numbers do you convert: ")
+		fmt.Scanln(&UserChoice)
 
-	switch UserChoice {
-	case "Decimal":
-		fmt.Println("You chose Decimal to Binary")
-		decimalToBinary(0)
-	case "Binary":
-		fmt.Println("You chose Binary to Decimal")
-		binaryToDecimal(0)
-	default:
-		fmt.Println("Invalid choice")
-		return
+		switch UserChoice {
+		case "Decimal":
+			fmt.Print("Give your decimal number: ")
+			var decimalNumber int
+			fmt.Scanln(&decimalNumber)
+			fmt.Println("Your binary number is:", decimalToBinary(decimalNumber))
+		case "Binary":
+			fmt.Print("Give your binary number: ")
+			var bainaryNumber string
+			fmt.Scanln(&bainaryNumber)
+			fmt.Println("Your binary number is:", binaryToDecimal(bainaryNumber))
+		case "Exit":
+			return
+		default:
+			fmt.Println("Invalid choice")
+		}
 	}
-
-	fmt.Println(UserChoice)
 }
